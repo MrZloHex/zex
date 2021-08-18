0fcb9fd5c2e9d42e97c4e3b498b31e97a644a1b2
MrZloHex <zlo.alex.it@gmail.com>
2021-08-18 23:43:56 +0300

    qwe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use clap::{App, load_yaml};
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from(yaml).get_matches();
+    if let Some(file) = matches.value_of("file") {
+        println!("File for open is {}", file);
+    };
 }

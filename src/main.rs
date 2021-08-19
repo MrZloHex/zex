c864b32279c0ffa93fd9c0a1a61964dc884b3741
MrZloHex <zlo.alex.it@gmail.com>
2021-08-19 12:17:09 +0300

    READ

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use clap::{App, load_yaml};
 
+use std::fs::{self, File};
+use std::io::Read;
+
+fn get_file_as_byte_vec(filename: &str) -> Vec<u8> {
+    let mut f = File::open(&filename).expect("no file found");
+    let metadata = fs::metadata(&filename).expect("unable to read metadata");
+    let mut buffer = vec![0; metadata.len() as usize];
+    f.read(&mut buffer).expect("buffer overflow");
+
+    buffer
+}
+
 fn main() {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from(yaml).get_matches();
-    if let Some(file) = matches.value_of("file") {
-        println!("File for open is {}", file);
+    let filename = if let Some(file) = matches.value_of("file") {
+        file
+    } else {
+        panic!();
     };
+
+    for char in get_file_as_byte_vec(filename) {
+        println!("{:X}", char);
+    }
 }
